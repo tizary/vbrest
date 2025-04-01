0e817daae979b780115ea63ebde50fccb0820947
tizary <tizary@mail.ru>
2025-04-01 23:57:31 +0300

    add poster page

--- vbrest/app/(tabs)/poster.tsx
+++ vbrest/app/(tabs)/poster.tsx
@@ -1,10 +1,28 @@
 import { AppColors } from "@/constants/colors"
-import { View, Text, StyleSheet } from "react-native"
+import { View, Text, StyleSheet, FlatList, Image } from "react-native"
+import jsonData from "../../assets/data-api.json"
+
 
 export default function Poster() {
+  const newsData: Data[] = jsonData.data
+
+  const renderNewsItem = ({ item }: { item: Data }) =>
+       (<>
+          <Text style={[styles.title, { fontSize: 16, marginBottom: 14, marginTop: 16 }]}>
+            {item.date}
+          </Text>
+          <Image source={{ uri: item.img_main }} style={{ height: 382 }} resizeMode="cover" />
+       </>
+      )
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Афиша</Text>
+       <FlatList
+              data={newsData}
+              renderItem={renderNewsItem}
+              keyExtractor={(item) => item.id}
+            />
     </View>
   )
 }
